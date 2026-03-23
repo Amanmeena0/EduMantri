d08f448741d98fee2b481181a0d91ed15a93c213
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-03-23 22:13:00 +0530

    updates

--- Server/mcp_tools/docTools/fetch.py
+++ Server/mcp_tools/docTools/fetch.py
@@ -1,6 +1,9 @@
 import os 
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
+import logging
+from typing import List, Dict, Any
+
 
 def load_documents() -> List[Documents]:
     """Load all documents from specified directories"""
@@ -18,4 +21,16 @@ def load_documents() -> List[Documents]:
         for file_path in Path(folder).rglob("*"):
             if file_path.suffix.lower() in ['.pdf','.txt']:
                 try:
-                    loader = PyPDFLoader(str(file_path)) if file_path
+                    loader = PyPDFLoader(str(file_path)) if file_path.suffix = ".pdf"  else TextLoader(str(file_path))
+                    docs = loader.load()
+
+                    for doc in docs:
+                        doc.metadata['file_path'] = str(file_path)
+                        doc.metadata['file_path'] = file_path.suffix
+
+
+                    all_documents.extend(docs)
+
+                except Exception as e:
+                    logger.warning(f"Failed ot load {file_path}: {e}")
+
