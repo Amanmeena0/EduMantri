65f009f3ed7b478677b17e31bc4670bd92431cc5
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-03-26 11:21:11 +0530

    Refactor embeddings.py to define Model class and organize local and query embeddings

--- Server/Processes/embeddings.py
+++ Server/Processes/embeddings.py
@@ -1,11 +1,10 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from sentence_transformers import SentenceTransformer
 
+class Model:
 
+    # --> local embeddings
+    LocalEmbeddings = model = SentenceTransformer('all-MiniLM-L6-v2')
 
-# --> local embeddings
-
-
-
-
-## query embeddings
-embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    ## query embeddings
+    QueryEmbeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
